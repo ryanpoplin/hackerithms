483a734766f2f6e5f65aedaa21b104b3647032c2
Byrdann Fox <mobileupstate@gmail.com>
2014-12-19 12:26:19 -0500

    Updates...

--- app.js
+++ app.js
@@ -6,11 +6,22 @@
 	var app = express();
 	var routes = require('./routes');
 	var errorHandlers = require('./middleware/errorhandlers');
+	var log = require('./middleware/log');
+
+	app.use(log.logger);
+	app.use(express.static(__dirname + '/static'));
 
 	app.get('/', routes.index);
 	app.get('/login', routes.login);
+	// ...
 	app.post('/login', routes.loginProcess);
 	app.get('/chat', routes.chat);
+	app.get('/error', function (req, res, next) {
+		next(new Error('It\'s contrived...'));
+	});
+
+	// ...
+	app.use(errorHandlers.error);
 	app.use(errorHandlers.notFound);
 
 	app.listen(3000);
--- middleware/errorhandlers.js
+++ middleware/errorhandlers.js
@@ -6,4 +6,9 @@
 		res.send(404, 'You are a dumb fucking fool!!!');
 	};
 
+	exports.error = function error (err, req, res, next) {
+		console.log(err);
+		res.send(500, 'Something fucked up...');
+	};
+
 }());
