0b6a81c061418026dd5e30bfa31e912ad729cc69
Byrdann Fox <mobileupstate@gmail.com>
2014-12-19 00:01:13 -0500

    Updates...

--- app.js
+++ app.js
@@ -4,13 +4,14 @@
 
 	var express = require('express');
 	var app = express();
+	var routes = require('./routes');
 
-	app.get('*', function(req, res) {
-		res.send('Express Response...');
-	});
+	app.get('/', routes.index);
+	app.get('/login', routes.login);
+	app.post('/login', routes.loginProcess);
+	app.get('/chat', routes.chat);
 
 	app.listen(3000);
-
 	console.log("App server running on port 3000...");
 
 }());
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+(function () {
+
+	"use strict";
+
+	module.exports.index = index;
+	module.exports.login = login;
+	module.exports.loginProcess = loginProcess;
+	module.exports.chat = chat;
+
+	function index (req, res) {
+		res.send('Index');
+	};
+
+	function login (req, res) {
+		res.send('Login');
+	};
+
+	function loginProcess (req, res) {
+		res.redirect('/');
+	};
+
+	function chat (req, res) {
+		res.send('Chat');
+	};
+
+}());
