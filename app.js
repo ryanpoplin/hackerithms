f7e2c982c86209fd8b131f3ef3899892fa8e338d
Byrdann Fox <mobileupstate@gmail.com>
2014-12-18 23:33:18 -0500

    Updates...

---
+++ app.js
@@ -0,0 +1,16 @@
+(function () {
+
+	"use strict";
+
+	var express = require('express');
+	var app = express();
+
+	app.get('*', function(req, res) {
+		res.send('Express Response...');
+	});
+
+	app.listen(3000);
+
+	console.log("App server running on port 3000...");
+
+}());
