be042840b3fda4e83ffa5df3d16eb5dd7ef598a4
Byrdann Fox <mobileupstate@gmail.com>
2014-12-20 15:36:42 -0500

    Updates...

--- app.js
+++ app.js
@@ -2,32 +2,24 @@
 
 	"use strict";
 
-	// the order counts here...
-
 	var express = require('express');
 	var routes = require('./routes');
 	var errorHandlers = require('./middleware/errorhandlers');
 	var log = require('./middleware/log');
-	// ...
 	var partials = require('express-partials');
-	// ...
 	var cookieParser = require('cookie-parser');
 	var session = require('express-session');
-	// connect-redis 'extends' the session...
 	var RedisStore = require('connect-redis')(session);
 
 	var app = express();
 
-	// ...
 	app.set('view options', {defaultLayout: 'layout'});
 	app.set('view engine', 'ejs');
 
-	// ...
 	app.use(partials());
 	app.use(log.logger);
 	app.use(express.static(__dirname + '/static'));
 
-	// ...	
 	var cookieSecret = function cookieId () {
 		var text = '';
     	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -39,8 +31,6 @@
 	};
 
 	var secret = cookieSecret();
-
-	// ...
 	
 	app.use(cookieParser(secret));
 
@@ -56,23 +46,12 @@
 	app.get('/', routes.index);
 	app.get('/login', routes.login);
 
-	// ...
 	app.post('/login', routes.loginProcess);
 
 	app.get('/chat', routes.chat);
-	// ...
 	app.get('/error', function (req, res, next) {
 		next(new Error('It\'s contrived...'));
 	});
-
-	/*app.use(function(req, res, next) {
-		if (req.session.pageCount) {
-			req.session.pageCount += 1;
-		} else {
-			req.session.pageCount = 1;
-			next();
-		}
-	});*/
 
 	app.use(errorHandlers.error);
 	app.use(errorHandlers.notFound);
--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,19 @@
 
 	"use strict";
 
-	// ...
-	// module.exports.index = index;
-	// module.exports.login = login;
-	// module.exports.loginProcess = loginProcess;
-	// module.exports.chat = chat;
-
-	// already setup to use the views folder...
 	exports.index = function index (req, res) {
-		// res.send('Index');
 		res.render('index', {title: 'Index'});
 	};
 
 	exports.login = function login (req, res) {
-		// res.send('Login');
 		res.render('login', {title: 'Login'});
 	};
 
-	// ...
 	exports.loginProcess = function loginProcess (req, res) {
 		res.redirect('/');
 	};
 
 	exports.chat = function chat (req, res) {
-		// res.send('Chat');
 		res.render('chat', {title: 'Chat'});
 	};
 
