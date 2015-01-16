f1bf31d33e071e1373023a1ae921af89deebaf66
Byrdann Fox <mobileupstate@gmail.com>
2015-01-16 03:00:36 -0500

    More JS array algo shit...

--- dsalgos/testaa.js
+++ dsalgos/testaa.js
@@ -32,8 +32,46 @@
 
 	};
 
-	fibonacciSequence(24);
+	fibonacciSequence(6);
 
-	// ...
+	// adding and removing elements form arrays...
+
+	var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+	// our 11th element, etc...
+	numbers[numbers.length] = 10;
+
+	numbers.push(11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24);
+
+	console.log(numbers);
+
+    // here's a good point to remember about the 0th indexing...
+
+    console.log(numbers.length);
+
+    console.log(numbers.length - 1);
+
+	// add new elements to the beginning of the array...
+
+    for (var i = numbers.length; i >= 0; i -= 1) {
+
+        // numbers[i] === numbers[25], and in JS you can dynamically do this...
+        // not in compiled languages usually...
+
+        numbers[i] = numbers[i - 1];
+
+    }
+
+    numbers[0] = -1;
+
+    console.log(numbers);
+
+    numbers.unshift(-2, -3, -4, -5, -6);
+
+    numbers.pop();
+
+    console.log(numbers);
+
+    
 
 }());
